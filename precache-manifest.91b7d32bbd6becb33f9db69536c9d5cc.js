48f57bf569d7ad061367471060faee76245ff1e2
germanpagano <sergiogermanpagano@gmail.com>
2022-06-27 14:47:45 -0300

    Updates

---
+++ precache-manifest.91b7d32bbd6becb33f9db69536c9d5cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "323700b72d5c359789c4",
+    "url": "/React-Clicks-Counter/static/css/main.94093420.chunk.css"
+  },
+  {
+    "revision": "323700b72d5c359789c4",
+    "url": "/React-Clicks-Counter/static/js/main.323700b7.chunk.js"
+  },
+  {
+    "revision": "0cf5f86c9763b075b409",
+    "url": "/React-Clicks-Counter/static/js/1.0cf5f86c.chunk.js"
+  },
+  {
+    "revision": "9944cd9f8e8b5a713365",
+    "url": "/React-Clicks-Counter/static/js/runtime~main.9944cd9f.js"
+  },
+  {
+    "revision": "c00d94842ae093a74ce52becdeacb98b",
+    "url": "/React-Clicks-Counter/static/media/freecodecamp-logo.c00d9484.png"
+  },
+  {
+    "revision": "d8ed458e4b897b483b61029e0b3cc571",
+    "url": "/React-Clicks-Counter/index.html"
+  }
+];
